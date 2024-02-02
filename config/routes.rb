85428c550e8ab6b8846f8b1621cfd921ce9abe24
jrletner <jrletner@gmail.com>
2024-02-01 19:04:59 -0600

    joes lesson update

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
+
+  #BLOGS
+  # http://localhost:3000/blogs/
   get 'blogs/', to: 'blogs#index'
-
+  # http://localhost:3000/blogs/1
   get 'blogs/:id', to: 'blogs#show'
-
+  # http://localhost:3000/blogs/
   post 'blogs/', to: 'blogs#create'
-
+  # http://localhost:3000/blogs/
   patch 'blogs/', to: 'blogs#update'
-
+  # http://localhost:3000/blogs/1
   delete 'blogs/:id', to: 'blogs#destroy'
-
+  # http://localhost:3000/blogs/blogCategory
   post 'blogs/blogCategory', to: 'blogs#blogCategory'
-
+  # http://localhost:3000/blogs/blogCategory/1
   get 'blogs/blogCategory/:id', to: 'blogs#getBlogCategory'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
 end
